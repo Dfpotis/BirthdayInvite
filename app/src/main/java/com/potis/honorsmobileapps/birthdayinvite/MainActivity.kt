17e97b7ab284cb49c9b352d44cb851e91faaad54
Dominic Potis <potisd22@hsnet.ahsd.org>
2021-11-23 11:35:32 -0500

    Initial commit

---
+++ app/src/main/java/com/potis/honorsmobileapps/birthdayinvite/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.potis.honorsmobileapps.birthdayinvite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
